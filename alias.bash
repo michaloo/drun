4afd71666718e4ae5ba0c827d3892effdcd00e0f
Michał Rączka <michal@raczka.me>
2017-04-27 01:42:36 +0200

    Update alias.bash

--- alias.bash
+++ alias.bash
@@ -1,3 +1,3 @@
 # Docker one-off command alias:
 # https://github.com/michaloo/drun
-alias drun='docker run -u $(id -u):$(id -g) -v ~/.drun:/drun -e HOME=/drun -v `pwd`:/app --workdir /app -it --rm'
+alias drun='docker run -u $(id -u):$(id -g) -v ~/.drun:/drun -e HOME=/drun -v `pwd`:/app --workdir /app -it --rm --network=host'
